862369c664a4eba8c768003c83017917d62ddc0b
2jcortesj2 <jjose.cortes@udea.edu.co>
2026-01-30 16:43:18 -0500

    fix: actualizar versión v1.1.0 en la pantalla de inicio (splash)

--- app/lib/screens/splash_screen.dart
+++ app/lib/screens/splash_screen.dart
@@ -177,7 +177,7 @@ class _SplashScreenState extends State<SplashScreen>
                   ),
                   const SizedBox(height: 24),
                   Text(
-                    'v1.0.1',
+                    'v1.1.0',
                     style: TextStyle(
                       color: Colors.grey[400],
                       fontSize: 14,
